5e1cf2f548c2f2e6c385de7a070c620d7a237b3f
emmadal <emmanueldalougou@gmail.com>
2022-12-17 10:24:30 +0000

    add global app theme

--- App.tsx
+++ App.tsx
@@ -17,6 +17,8 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {Provider} from 'react-native-paper';
+import theme from './src/themes';
 
 const Section: React.FC<
   PropsWithChildren<{
@@ -60,36 +62,42 @@ const App = () => {
   }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Provider
+      theme={{
+        ...theme,
+        version: 3,
+      }}>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <Header />
+          <View
+            style={{
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            }}>
+            <Section title="Step One">
+              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+              screen and then come back to see your edits.
+            </Section>
+            <Section title="See Your Changes">
+              <ReloadInstructions />
+            </Section>
+            <Section title="Debug">
+              <DebugInstructions />
+            </Section>
+            <Section title="Learn More">
+              Read the docs to discover what to do next:
+            </Section>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Provider>
   );
 };
 
---
+++ src/themes/index.ts
@@ -0,0 +1,21 @@
+import {MD3LightTheme as DefaultTheme} from 'react-native-paper';
+import {DefaultTheme as NavigationDefaultTheme} from '@react-navigation/native';
+
+const theme = {
+  ...DefaultTheme,
+  ...NavigationDefaultTheme,
+  myOwnProperty: true,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#0752B6',
+    success: '#4BB543',
+    error: '#F44335',
+    light: '#FFFFFF',
+    clouds: '#ecf0f1',
+    text: '#000000',
+    grey100: '#00000084',
+    snack: '#4d6e80',
+  },
+};
+
+export default theme;

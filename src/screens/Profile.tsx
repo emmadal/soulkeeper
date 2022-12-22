29d0d4f83cceb736623d3d54b96ae2cdcff5cfdb
emmadal <emmanueldalougou@gmail.com>
2022-12-22 12:27:59 +0000

    add Profile tempalte screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Text} from 'react-native-paper';
-import {ScrollView, SafeAreaView, StyleSheet} from 'react-native';
+import {Text, Avatar, TextInput} from 'react-native-paper';
+import {ScrollView, SafeAreaView, StyleSheet, View} from 'react-native';
 import theme from '../themes';
 
 const Profile = () => {
@@ -10,7 +10,47 @@ const Profile = () => {
         showsHorizontalScrollIndicator={false}
         contentInsetAdjustmentBehavior="automatic"
         contentContainerStyle={styles.contentScroll}>
-        <Text variant="titleLarge">Profile Screen</Text>
+        <View style={styles.header}>
+          <Avatar.Text label="BD" size={70} />
+          <Text variant="titleLarge">Big Data</Text>
+        </View>
+        <View style={styles.form}>
+          <TextInput
+            autoCapitalize="none"
+            style={styles.input}
+            label="Nom"
+            mode="outlined"
+          />
+          <TextInput
+            autoCapitalize="none"
+            style={styles.input}
+            label="Prénoms"
+            mode="outlined"
+          />
+          <TextInput
+            autoCapitalize="none"
+            style={styles.input}
+            label="Email"
+            mode="outlined"
+          />
+          <TextInput
+            style={styles.input}
+            label="Nom du responsable"
+            mode="outlined"
+          />
+          <TextInput
+            autoCapitalize="none"
+            style={styles.input}
+            label="Expediteur"
+            mode="outlined"
+          />
+          <TextInput
+            autoCapitalize="none"
+            style={styles.input}
+            label="Telephone"
+            mode="outlined"
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -24,6 +64,18 @@ const styles = StyleSheet.create({
   contentScroll: {
     flexGrow: 1,
     padding: 20,
+  },
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  form: {
+    marginTop: 25,
+  },
+  input: {
+    textAlign: 'auto',
+    marginVertical: 10,
   },
 });
 export default Profile;

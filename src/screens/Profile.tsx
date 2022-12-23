968163e7af969dce0629db61a82069695b6dc1fd
emmadal <emmanueldalougou@gmail.com>
2022-12-23 22:29:49 +0000

    add info in Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,54 +1,84 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Text, Avatar, TextInput} from 'react-native-paper';
 import {ScrollView, SafeAreaView, StyleSheet, View} from 'react-native';
 import theme from '../themes';
+import {AuthContext} from '../context/AuthContext';
 
 const Profile = () => {
+  const {state} = useContext(AuthContext);
+
+  const renderAvatarName = (name: string) => {
+    const characters = name?.split(' ');
+    const firstChar = characters[0]?.charAt(0);
+    const secondChar = characters[1]?.charAt(0);
+    return `${firstChar}${secondChar}`.toUpperCase();
+  };
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView
-        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
         contentInsetAdjustmentBehavior="automatic"
         contentContainerStyle={styles.contentScroll}>
         <View style={styles.header}>
-          <Avatar.Text label="BD" size={70} />
-          <Text variant="titleLarge">Big Data</Text>
+          <Avatar.Text
+            label={renderAvatarName(state.user.nomentreprise)}
+            size={70}
+          />
+          <Text variant="titleLarge" style={styles.name}>
+            {state.user.nomentreprise}
+          </Text>
+          <Text variant="bodyLarge" style={styles.desc}>
+            {state.user.description ?? 'Aucune description disponible'}
+          </Text>
         </View>
         <View style={styles.form}>
           <TextInput
             autoCapitalize="none"
             style={styles.input}
-            label="Nom"
+            label="Nom d'utilisateur"
             mode="outlined"
+            value={state.user.login}
           />
           <TextInput
             autoCapitalize="none"
             style={styles.input}
-            label="Prénoms"
+            label="Entreprise"
             mode="outlined"
+            value={state.user.nomentreprise}
           />
           <TextInput
             autoCapitalize="none"
             style={styles.input}
             label="Email"
             mode="outlined"
+            value={state.user.email}
           />
           <TextInput
             style={styles.input}
             label="Nom du responsable"
             mode="outlined"
+            value={state.user.nomresponsable}
           />
           <TextInput
             autoCapitalize="none"
             style={styles.input}
             label="Expediteur"
             mode="outlined"
+            value={state.user.expediteur}
           />
           <TextInput
             autoCapitalize="none"
             style={styles.input}
             label="Telephone"
             mode="outlined"
+            value={state.user.numero}
+          />
+          <TextInput
+            autoCapitalize="none"
+            style={styles.input}
+            label="Autre Contact"
+            mode="outlined"
+            value={state.user.numerotwo ?? ''}
           />
         </View>
       </ScrollView>
@@ -76,6 +106,13 @@ const styles = StyleSheet.create({
   input: {
     textAlign: 'auto',
     marginVertical: 10,
+  },
+  name: {
+    fontWeight: '500',
+    marginVertical: 10,
+  },
+  desc: {
+    textAlign: 'center',
   },
 });
 export default Profile;
